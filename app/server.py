a24bef9fadf393a9ab60b07e3ebe940c23e7fd6e
dp5c14 <38218824+dp5c14@users.noreply.github.com>
2020-04-06 12:41:50 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1dBuWnUR9ki_qrex_62HKsXgRNNIhfFIY'
+export_file_url = 'https://www.dropbox.com/s/0gfg9nn0i6q241d/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
 classes = ['soccer', 'rugby', 'basket']
